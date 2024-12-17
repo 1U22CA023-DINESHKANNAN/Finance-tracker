202cc3875a1a6b10937a11659df0ed4776dd1d55
1U22CA023-DINESHKANNAN <dineshkannan616@gmail.com>
2024-12-18 00:22:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import SignUP from "./views/login&signup/signup"
+import Login from "./views/login&signup/login"
+import Dashboard from "./views/dashboard"
+import Update from "./views/update"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/signup" element={<SignUP/>}/>
+        <Route path="/update" element={<Update/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
